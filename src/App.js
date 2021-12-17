2d6174b96e3482e60a0af7ce0c684c0a1de31959
Kim Nguyen <kimnivore@gmail.com>
2021-12-17 10:00:54 -0800

    passed test 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, Route } from 'react-router-dom';
+import PizzaForm from './PizzaForm';
+import Pizza from './Pizza';
+
+const initialFormValues = {
+
+}
 
 const App = () => {
   return (
-    <>
+    <div>
+      <nav id="order-pizza">
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+        <Link to='/'>Home</Link>
+        <Link to='/pizzaForm'>Pizza</Link>
+      </nav>
+
+      <Route exact path ='/'>
+      </Route>
+      
+      <Route path='/pizzaForm'>
+          <PizzaForm />
+      </Route>
+    </div>
   );
 };
 export default App;
---
+++ src/PizzaForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const PizzaForm = (props) => {
+
+    // const onChange = evt => {
+    //     const name = evt.target.name;
+    //     const {value} = evt.target;
+    //     update(name, value);
+    // }
+
+    // const onSubmit = evt => {
+    //     evt.preventDefault();
+    //     submit();
+    // }
+    return (
+       <form id='pizza-form'>
+           <label>Name
+               <input
+
+               />
+           </label>
+
+       </form>
+
+    )
+}
+
+
+export default PizzaForm;
